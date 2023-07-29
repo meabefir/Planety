d0f72bdf48b0fa95ae4ea27445edc46aff291c1d
DenisaTerchescu <dterchescu@yahoo.com>
2023-07-29 12:50:49 +0300

    added a cute lil function for spawning the potions

--- scenes/gameplay/Potion.gd
+++ scenes/gameplay/Potion.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-var startPos = Vector2.ZERO
-var endPos = startPos + Vector2(0, -100)
+
+export var startPos : Vector2 = position
+
 func _ready() -> void:
-	$GoUpTween.interpolate_property(self, "position", startPos, endPos, 5, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	var endPos = startPos + Vector2(0, -100)
+	$GoUpTween.interpolate_property(self, "position", position, endPos, 5, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	$GoUpTween.interpolate_property(self, "rotation_degrees", 0, 5, 5, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	$GoUpTween.start()
 
-func _on_GoUpTween_tween_completed(object: Object, key: NodePath) -> void:
-	 $GoDownTween.interpolate_property(self, "position", endPos, startPos, 5, Tween.TRANS_QUAD, Tween.EASE_IN)
+func _on_GoUpTween_tween_completed(_object: Object, _key: NodePath) -> void:
+	 $GoDownTween.interpolate_property(self, "position", position, startPos, 5, Tween.TRANS_QUAD, Tween.EASE_IN)
 	 $GoDownTween.interpolate_property(self, "rotation_degrees", 5, 0, 5, Tween.TRANS_QUAD, Tween.EASE_IN)
 	 $GoDownTween.start()
--- scenes/gameplay/planet.gd
+++ scenes/gameplay/planet.gd
@@ -4,3 +4,21 @@ class_name Planet
 
 onready var sprite = get_node("Sprite")
 export var radius: float = 100
+var spawn_timer = Timer.new()
+onready var potionScene  = preload("res://scenes/gameplay/Potion.tscn")
+var timer = 0.0  
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	spawnPotions(delta)
+	
+func spawnPotions(delta: float):
+	var potion = potionScene.instance()
+	timer += delta
+	if timer >= 3:
+		potion.startPos = Vector2.ZERO + Vector2(0, -1.5*radius)
+		add_child(potion)
+		timer = 0.0 
+	
