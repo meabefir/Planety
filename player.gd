057a22210aaa921d75df3cb2b5d74575426cf27b
Petru Huja <58290933+meabefir@users.noreply.github.com>
2023-07-29 03:01:42 +0300

    planet orbiter script

--- player.gd
+++ player.gd
@@ -8,6 +8,8 @@ var m_velocity: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	m_planet = get_parent()
+	
 	m_currentAngle = -PI / 2
 
 func _process(delta: float) -> void:
@@ -17,18 +19,20 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("right"):
 		vec_input.x += 1
 	
+	m_horizontalVelocity = vec_input.x * SPEED
+	m_verticalVelocity = m_velocity.y
+	
 	if Input.is_action_just_pressed("jump"):
 		if m_height == 0:
 			jump(delta)
-	
-	print(m_height, " ", m_velocity.y)
+			return
+			
+#	print(m_height, " ", m_velocity.y)
 	if m_height > 0:
 		m_velocity.y -= G * delta
 	else:
 		m_velocity.y = 0
-	print(m_velocity.y)
-	m_horizontalVelocity = vec_input.x * SPEED
-	m_verticalVelocity = m_velocity.y
+#	print(m_velocity.y)
 	
 func jump(delta):
 	m_velocity.y = JUMP_FORCE
